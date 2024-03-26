81acfa1d6b2d021012335cb7894bf92e3196dc47
skyDe <sky_de1991@outlook.com>
2024-03-26 04:10:29 -0800

    responsiveness issue fixed

--- src/scenes/Dashboard.jsx
+++ src/scenes/Dashboard.jsx
@@ -1,65 +1,68 @@
-import { mockStatData  } from "../data/mockData";
+import { mockStatData } from "../data/mockData";
 import { Box } from "@mui/material";
 import {
-  StatCard ,
+  StatCard,
   LineChartCard,
   TransactionList,
   CampaignCard,
   BarChartCard,
-  GeographyChartCard
+  GeographyChartCard,
 } from "../components/dashboardCards";
-
 
 const Dashboard = () => {
   return (
     // Grid
-    <Box 
-      maxWidth="100%" 
+    <Box
+      maxWidth="100%"
       maxHeight="82vh"
-      display="grid" 
-      gap="20px" 
-      p="1rem" 
+      display="grid"
+      gap="20px"
+      p="1rem"
       gridTemplateColumns="repeat(12,1fr)"
       gridAutoRows="140px"
-      sx={{overflow:{ xs: "auto", xl: "hidden"}}}
-     >
-
+      sx={{ overflow: { xs: "auto" } }}
+    >
       {/* Row 1 ----------------------------------------------------*/}
-      {mockStatData?.map( stats => ( 
-        <StatCard 
+      {mockStatData?.map((stats) => (
+        <StatCard
           key={stats.id}
           {...stats}
           xsGridColumn="span 12"
           smGridColumn="span 12"
           mdGridColumn="span 6"
-          lgGridColumn="span 3" />
-       ))}
-      
+          lgGridColumn="span 3"
+        />
+      ))}
+
       {/* Row 2 --------------------------------------------------------*/}
-      <LineChartCard xsGridColumn="span 12" mdGridColumn="span 8" /> 
-      <TransactionList xsGridColumn="span 12" smGridColumn="span 6" mdGridColumn="span 4" />
+      <LineChartCard xsGridColumn="span 12" mdGridColumn="span 8" />
+      <TransactionList
+        xsGridColumn="span 12"
+        smGridColumn="span 6"
+        mdGridColumn="span 4"
+      />
 
       {/* Row 3 -------------------------------------------------------------- */}
       <CampaignCard
-       xsGridColumn="span 12" 
-       smGridColumn="span 6" 
-       mdGridColumn="span 6" 
-       xlGridColumn="span 4"/>
+        xsGridColumn="span 12"
+        smGridColumn="span 6"
+        mdGridColumn="span 6"
+        xlGridColumn="span 4"
+      />
 
       <BarChartCard
-       xsGridColumn="span 12" 
-       mdGridColumn="span 6" 
-       xlGridColumn="span 4"/>
-       
-      <GeographyChartCard  
-       xsGridColumn="span 12" 
-       mdGridColumn="span 12" 
-       xlGridColumn="span 4"/>
+        xsGridColumn="span 12"
+        mdGridColumn="span 6"
+        xlGridColumn="span 4"
+      />
+
+      <GeographyChartCard
+        xsGridColumn="span 12"
+        mdGridColumn="span 12"
+        xlGridColumn="span 4"
+      />
     </Box>
-  )
-}
+  );
+};
 
-export default Dashboard
-
-
-
+export default Dashboard;
--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,53 +1,60 @@
 import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
-import { ColorModeContext  } from "../../theme";
+import { ColorModeContext } from "../../theme";
 import InputBase from "@mui/material/InputBase";
-import { 
-  LightModeOutlined, 
-  DarkModeOutlined, 
-  NotificationsOutlined, 
-  SettingsOutlined, 
-  PersonOutline, 
-  Search } from "@mui/icons-material";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  NotificationsOutlined,
+  SettingsOutlined,
+  PersonOutline,
+  Search,
+} from "@mui/icons-material";
 import { useColors } from "../../hooks/useColors";
 
 const Topbar = () => {
   const theme = useTheme();
   const { colors } = useColors();
   const colorMode = useContext(ColorModeContext);
-  
+
   return (
-    <Box display="flex" justifyContent="space-between" p={2}>
+    <Box display="flex" justifyContent="space-between" py={1} px={2}>
       {/* SEARCH BAR */}
-      <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
         <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
         <IconButton>
-          <Search/>
+          <Search />
         </IconButton>
       </Box>
 
       {/* ICONS */}
       <Box display="flex">
-
         <IconButton onClick={colorMode.toggleColorMode}>
-          {theme.palette.mode === "dark" ? <LightModeOutlined/> : <DarkModeOutlined/>}
+          {theme.palette.mode === "dark" ? (
+            <LightModeOutlined />
+          ) : (
+            <DarkModeOutlined />
+          )}
         </IconButton>
 
         <IconButton>
-          <NotificationsOutlined/>
+          <NotificationsOutlined />
         </IconButton>
 
         <IconButton>
-          <SettingsOutlined/>
+          <SettingsOutlined />
         </IconButton>
 
         <IconButton>
-          <PersonOutline/>
+          <PersonOutline />
         </IconButton>
-
       </Box>
     </Box>
-  )
-}
+  );
+};
 
 export default Topbar;
